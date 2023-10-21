283699e956aa0f7d98548812f719a36356e19205
ZeynepKlc <109767063+ZeynepKlc@users.noreply.github.com>
2023-10-22 00:03:45 +0300

    file paths in folder

---
+++ data_paths.py
@@ -0,0 +1,21 @@
+CLOUDY_SOURCE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\cloudy\\'
+TRAINING_CLOUDY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\train\\cloudy\\'
+VALID_CLOUDY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\validation\\cloudy\\'
+
+FOGGY_SOURCE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\foggy\\'
+TRAINING_FOGGY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\train\\foggy\\'
+VALID_FOGGY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\validation\\foggy\\'
+
+RAINY_SOURCE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\rainy\\'
+TRAINING_RAINY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\train\\rainy\\'
+VALID_RAINY_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\validation\\rainy\\'
+
+SHINE_SOURCE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\shine\\'
+TRAINING_SHINE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\train\\shine\\'
+VALID_SHINE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\validation\\shine\\'
+
+SUNRISE_SOURCE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\sunrise\\'
+TRAINING_SUNRISE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\train\\sunrise\\'
+VALID_SUNRISE_DIR = 'C:\\Users\\hp\\Desktop\\weatherdataset\\weather_pred\\validation\\sunrise\\'
+
+
